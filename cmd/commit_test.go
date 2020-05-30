b935dcfe72b820ff834252e4e3378822094f47f2
bastien <bastien@loftorbital.com>
2020-05-30 11:22:34 +0200

    test(commit): add commit test
    Reference #14

--- cmd/commit_test.go
+++ cmd/commit_test.go
@@ -27,3 +27,44 @@ func TestWriteCommit(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, "commit message", c.Message)
 }
+
+func TestCommit(t *testing.T) {
+	r, teardown, err := setUpRepo()
+	defer teardown()
+	require.NoError(t, err)
+
+	cmd := &cobra.Command{}
+
+	fType := cmd.Flags().StringP("type", "t", "", "")
+	fBreak := cmd.Flags().BoolP("breaking-changes", "c", false, "")
+	cmd.Flags().BoolP("edit", "e", false, "")
+	fScope := cmd.Flags().StringP("scope", "s", "", "")
+
+	assert.Error(t, commit(cmd, []string{"not-type"}))
+
+	assertLastCommit := func(msg string) {
+		citer, err := r.Log(&git.LogOptions{})
+		require.NoError(t, err)
+		c, err := citer.Next()
+		require.NoError(t, err)
+		assert.Equal(t, msg, c.Message)
+	}
+
+	*fType = "feat"
+	*fBreak = false
+	*fScope = ""
+	assert.NoError(t, commit(cmd, []string{"my", "message"}))
+	assertLastCommit("feat: my message")
+
+	*fType = ""
+	*fBreak = true
+	*fScope = ""
+	assert.NoError(t, commit(cmd, []string{"fix", "my", "message"}))
+	assertLastCommit("fix!: my message")
+
+	*fType = ""
+	*fBreak = false
+	*fScope = "scope"
+	assert.NoError(t, commit(cmd, []string{"test", "my", "message"}))
+	assertLastCommit("test(scope): my message")
+}
