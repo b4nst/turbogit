0b36f733be724f45ec33defce2ea4968ed9862b0
banst <bastyen.a@gmail.com>
2021-01-24 14:49:11 +0100

    test(branch): add parse option tests

--- cmd/branch_test.go
+++ cmd/branch_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/b4nst/turbogit/internal/format"
 	"github.com/libgit2/git2go/v30"
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -48,4 +49,44 @@ func TestBranchCreate(t *testing.T) {
 	h, err := r.Head()
 	require.NoError(t, err)
 	assert.Equal(t, "refs/heads/"+bco.Name, h.Name())
+}
+
+func TestParseBranchCmd(t *testing.T) {
+	dir, err := ioutil.TempDir("", "turbogit-test-commit")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	require.NoError(t, os.Chdir(dir))
+
+	r, err := git.InitRepository(dir, false)
+	require.NoError(t, err)
+	config, err := r.Config()
+	require.NoError(t, err)
+	require.NoError(t, config.SetString("user.name", "alice"))
+
+	cmd := &cobra.Command{}
+
+	// Incorrect type
+	_, err = parseBranchCmd(cmd, []string{"nope"})
+	assert.Error(t, err, "Nope")
+	// Not user no description
+	_, err = parseBranchCmd(cmd, []string{"feat"})
+	assert.Error(t, err, "feat branches need a description.")
+	// User branch
+	bco, err := parseBranchCmd(cmd, []string{"user"})
+	assert.NoError(t, err)
+	expected := BranchCmdOption{
+		BType: format.UserBranch,
+		Name:  "user/alice",
+		Repo:  r,
+	}
+	assert.Equal(t, expected, *bco)
+	// Classic branch
+	bco, err = parseBranchCmd(cmd, []string{"feat", "Foo", "bAr"})
+	assert.NoError(t, err)
+	expected = BranchCmdOption{
+		BType: format.FeatureBranch,
+		Name:  "feat/foo-bar",
+		Repo:  r,
+	}
+	assert.Equal(t, expected, *bco)
 }
