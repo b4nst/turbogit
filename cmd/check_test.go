c47e0f36f8ae5009d6e4868d67818f4c689564b0
banst <bastyen.a@gmail.com>
2021-01-31 23:45:33 +0100

    refactor(check)!: use git2go for check command

--- cmd/check_test.go
+++ cmd/check_test.go
@@ -2,89 +2,39 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/b4nst/turbogit/internal/test"
+	"github.com/libgit2/git2go/v30"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
 	"testing"
-
-	"github.com/b4nst/turbogit/internal/format"
-	"github.com/b4nst/turbogit/internal/test"
-	"github.com/spf13/cobra"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestCheckCommit(t *testing.T) {
-	r, teardown, err := test.SetUpRepo()
-	defer teardown()
-	require.NoError(t, err)
-
-	c := test.AddCommit(t, r, "bad format")
-	stderr, reset := test.CaptureStd(t, os.Stderr)
-	ccr := checkCommit(c)
-	reset()
-	assert.False(t, ccr)
-	stde, err := ioutil.ReadFile(stderr.Name())
-	assert.Equal(t, fmt.Sprintf("%s %s\n", c.Hash, "bad format"), string(stde))
-
-	c = test.AddCommit(t, r, format.CommitMessage(&format.CommitMessageOption{Ctype: format.BuildCommit, Description: "correct format"}))
-	ccr = checkCommit(c)
-	assert.True(t, ccr)
-}
-
 func TestRunCheck(t *testing.T) {
-	r, teardown, err := test.SetUpRepo()
-	defer teardown()
+	dir, err := ioutil.TempDir("", "turbogit-test-check")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	require.NoError(t, os.Chdir(dir))
+
+	r, err := git.InitRepository(dir, false)
 	require.NoError(t, err)
 
-	cmd := &cobra.Command{}
+	c1, err := writeCommit(r, "bad commit 1")
+	require.NoError(t, err)
+	sid1, err := c1.ShortId()
+	require.NoError(t, err)
+	_, err = writeCommit(r, "feat: ok commit")
+	assert.NoError(t, err)
+	c3, err := writeCommit(r, "bad commit 2")
+	assert.NoError(t, err)
+	sid3, err := c3.ShortId()
+	require.NoError(t, err)
 
-	fAll := cmd.Flags().BoolP("all", "a", false, "Check all the refs in refs/, along with HEAD")
-	fFrom := cmd.Flags().StringP("from", "f", "HEAD", "Hash of the commit to start from")
-
-	// Add 3 commit (the first one is alread set by SetUpRepo)
-	from := test.AddCommit(t, r, format.CommitMessage(&format.CommitMessageOption{Ctype: format.BuildCommit, Description: "hello world"}))
-	test.AddCommit(t, r, format.CommitMessage(&format.CommitMessageOption{Ctype: format.FeatureCommit, Description: "right format"}))
-	test.AddCommit(t, r, format.CommitMessage(&format.CommitMessageOption{Ctype: format.CiCommit, Description: "correct format"}))
-	// Test with default option
-	*fAll = false
-	*fFrom = "HEAD"
-	stdout, reset := test.CaptureStd(t, os.Stdout)
-	err = runCheck(cmd, []string{})
-	reset()
-	assert.NoError(t, err)
-	stdo, err := ioutil.ReadFile(stdout.Name())
-	assert.Equal(t, "4 commit(s) checked.", string(stdo))
-	// Test from
-	*fAll = false
-	*fFrom = from.Hash.String()
-	stdout, reset = test.CaptureStd(t, os.Stdout)
-	err = runCheck(cmd, []string{})
-	reset()
-	assert.NoError(t, err)
-	stdo, err = ioutil.ReadFile(stdout.Name())
-	assert.Equal(t, "2 commit(s) checked.", string(stdo))
-	// Test from relative
-	*fAll = false
-	*fFrom = "HEAD~2"
-	stdout, reset = test.CaptureStd(t, os.Stdout)
-	err = runCheck(cmd, []string{})
-	reset()
-	assert.NoError(t, err)
-	stdo, err = ioutil.ReadFile(stdout.Name())
-	assert.Equal(t, "2 commit(s) checked.", string(stdo))
-	// Test from bad
-	*fAll = false
-	*fFrom = "nope"
-	err = runCheck(cmd, []string{})
-	assert.EqualError(t, err, "Error looking for commit nope: reference not found")
-	// Test with bad commit
-	bad := test.AddCommit(t, r, "badbeef")
-	*fAll = false
-	*fFrom = "HEAD"
 	stderr, reset := test.CaptureStd(t, os.Stderr)
-	err = runCheck(cmd, []string{})
+	err = runCheck(&CheckCmdOption{All: false, From: "HEAD", Repo: r})
 	reset()
-	assert.EqualError(t, err, "The previous commit(s) do(es) not respect Conventional Commit.")
+	assert.EqualError(t, err, "This commits are not compliant")
 	stde, err := ioutil.ReadFile(stderr.Name())
-	assert.Equal(t, fmt.Sprintf("%s %s\n", bad.Hash, "badbeef"), string(stde))
+	assert.Equal(t, fmt.Sprintf("%s %s\n%s %s\n", sid3, "bad commit 2", sid1, "bad commit 1"), string(stde))
 }
