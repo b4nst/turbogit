94ef4177de3b4e244a75a1277476ed347ee0f8b1
Alice <alice@ecorp.com>
2021-03-14 23:54:02 +0100

    refactor: add git wrapper functions

---
+++ internal/git/commit.go
@@ -0,0 +1,70 @@
+package git
+
+import git "github.com/libgit2/git2go/v30"
+
+// RepoTree return the current index tree
+func RepoTree(r *git.Repository) (*git.Tree, error) {
+	idx, err := r.Index()
+	if err != nil {
+		return nil, err
+	}
+	treeId, err := idx.WriteTree()
+	if err != nil {
+		return nil, err
+	}
+	tree, err := r.LookupTree(treeId)
+	if err != nil {
+		return nil, err
+	}
+	return tree, nil
+}
+
+// Commit creates a new commit with the current tree.
+func Commit(r *git.Repository, msg string) (*git.Commit, error) {
+	// Signature
+	sig, err := r.DefaultSignature()
+	if err != nil {
+		return nil, err
+	}
+	// Tree
+	tree, err := RepoTree(r)
+	if err != nil {
+		return nil, err
+	}
+	// Parents
+	parents := []*git.Commit{}
+	head, err := r.Head()
+	if err == nil { // We found head
+		headRef, err := r.LookupCommit(head.Target())
+		if err != nil {
+			return nil, err
+		}
+		parents = append(parents, headRef)
+	}
+
+	oid, err := r.CreateCommit("HEAD", sig, sig, msg, tree, parents...)
+	if err != nil {
+		return nil, err
+	}
+	return r.LookupCommit(oid)
+}
+
+// Amend amends the HEAD commit
+func Amend(ca *git.Commit, msg string) (*git.Commit, error) {
+	r := ca.Object.Owner()
+	// Signature
+	sig, err := r.DefaultSignature()
+	if err != nil {
+		return nil, err
+	}
+	// Tree
+	tree, err := RepoTree(r)
+	if err != nil {
+		return nil, err
+	}
+	oid, err := ca.Amend("HEAD", ca.Author(), sig, msg, tree)
+	if err != nil {
+		return nil, err
+	}
+	return r.LookupCommit(oid)
+}
---
+++ internal/git/commit_test.go
@@ -0,0 +1,44 @@
+package git
+
+import (
+	"testing"
+	"time"
+
+	"github.com/b4nst/turbogit/internal/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommit(t *testing.T) {
+	r := test.TestRepo(t)
+	defer test.CleanupRepo(t, r)
+	test.InitRepoConf(t, r)
+
+	commit, err := Commit(r, "commit message")
+	assert.NoError(t, err)
+	assert.Equal(t, "commit message", commit.Message())
+	assert.Equal(t, test.GIT_USERNAME, commit.Author().Name)
+	assert.Equal(t, test.GIT_EMAIL, commit.Author().Email)
+	assert.WithinDuration(t, time.Now(), commit.Author().When, time.Second)
+	head, err := r.Head()
+	require.NoError(t, err)
+	headCommit, err := r.LookupCommit(head.Target())
+	require.NoError(t, err)
+	assert.Equal(t, headCommit.Id(), commit.Id())
+}
+
+func TestAmend(t *testing.T) {
+	r := test.TestRepo(t)
+	test.InitRepoConf(t, r)
+	commit, err := Commit(r, "foo")
+	require.NoError(t, err)
+
+	amendc, err := Amend(commit, "bar")
+	assert.NoError(t, err)
+	head, err := r.Head()
+	require.NoError(t, err)
+	headc, err := r.LookupCommit(head.Target())
+	require.NoError(t, err)
+	assert.Equal(t, "bar", headc.Message())
+	assert.Equal(t, headc.Id(), amendc.Id())
+}
