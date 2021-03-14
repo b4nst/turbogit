a726554e438fd132a6ca32898e234abaed946b37
Alice <alice@ecorp.com>
2021-03-14 23:54:02 +0100

    refactor: add git wrapper functions

---
+++ internal/git/stage.go
@@ -0,0 +1,33 @@
+package git
+
+import (
+	"errors"
+
+	git "github.com/libgit2/git2go/v30"
+)
+
+// StageReady returns true if the stage is ready to be committed. Otherwise it returns false if there is nothing to commit or an error.
+func StageReady(r *git.Repository) (bool, error) {
+	s, err := r.StatusList(&git.StatusOptions{Show: git.StatusShowIndexAndWorkdir, Flags: git.StatusOptIncludeUntracked})
+	if err != nil {
+		return false, err
+	}
+
+	count, err := s.EntryCount()
+	if err != nil {
+		return false, err
+	}
+	if count <= 0 {
+		return false, nil
+	}
+	for i := 0; i < count; i++ {
+		se, err := s.ByIndex(i)
+		if err != nil {
+			return false, err
+		}
+		if se.Status <= git.StatusIndexTypeChange {
+			return true, nil
+		}
+	}
+	return false, errors.New("No changes added to commit")
+}
---
+++ internal/git/stage_test.go
@@ -0,0 +1,26 @@
+package git
+
+import (
+	"testing"
+
+	"github.com/b4nst/turbogit/internal/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStageReady(t *testing.T) {
+	r := test.TestRepo(t)
+	defer test.CleanupRepo(t, r)
+
+	nc, err := StageReady(r)
+	assert.NoError(t, err)
+	assert.False(t, nc)
+
+	f := test.NewFile(t, r)
+	nc, err = StageReady(r)
+	assert.EqualError(t, err, "No changes added to commit")
+
+	test.StageFile(t, f, r)
+	nc, err = StageReady(r)
+	assert.NoError(t, err)
+	assert.True(t, nc)
+}
