6152683d7cdcf7c01fb12b12bffd164797ca4762
banst <bastyen.a@gmail.com>
2022-04-01 17:22:04 +0200

    feat: create git-new command

--- cmd/git-new/cmd/root_test.go
+++ cmd/git-new/cmd/root_test.go
@@ -16,14 +16,14 @@ func TestBranchCreate(t *testing.T) {
 	defer test.CleanupRepo(t, r)
 	test.InitRepoConf(t, r)
 
-	bco := &BranchCmdOption{
+	bco := &option{
 		format.TugBranch{Type: "feat", Description: "foo"},
 		r,
 	}
-	assert.Error(t, runBranch(bco), "No commit to create branch from, please create the initial commit")
+	assert.Error(t, run(bco), "No commit to create branch from, please create the initial commit")
 	tugit.Commit(r, "initial commit")
 
-	assert.NoError(t, runBranch(bco))
+	assert.NoError(t, run(bco))
 	h, err := r.Head()
 	require.NoError(t, err)
 	assert.Equal(t, "refs/heads/feat/foo", h.Name())
@@ -37,25 +37,25 @@ func TestParseBranchCmd(t *testing.T) {
 	cmd := &cobra.Command{}
 
 	// User branch
-	bco, err := parseBranchCmd(cmd, []string{"user", "my", "branch"})
+	bco, err := parseCmd(cmd, []string{"user", "my", "branch"})
 	assert.NoError(t, err)
-	expected := BranchCmdOption{
+	expected := option{
 		NewBranch: format.TugBranch{Type: "user", Prefix: test.GIT_USERNAME, Description: "my branch"},
 		Repo:      r,
 	}
 	assert.Equal(t, expected, *bco)
 	// Users branch
-	bco, err = parseBranchCmd(cmd, []string{"users", "my", "branch"})
+	bco, err = parseCmd(cmd, []string{"users", "my", "branch"})
 	assert.NoError(t, err)
-	expected = BranchCmdOption{
+	expected = option{
 		NewBranch: format.TugBranch{Type: "users", Prefix: test.GIT_USERNAME, Description: "my branch"},
 		Repo:      r,
 	}
 	assert.Equal(t, expected, *bco)
 	// Classic branch
-	bco, err = parseBranchCmd(cmd, []string{"feat", "foo", "bar"})
+	bco, err = parseCmd(cmd, []string{"feat", "foo", "bar"})
 	assert.NoError(t, err)
-	expected = BranchCmdOption{
+	expected = option{
 		NewBranch: format.TugBranch{Type: "feat", Description: "foo bar"},
 		Repo:      r,
 	}
